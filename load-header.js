2f607aa072b490fee16a1f27368c9ae6c5e8a23d
横山立門 <>
2026-04-18 15:45:57 +0900

    ヘッダーのリンクを正常にするための変更

--- load-header.js
+++ load-header.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const headerTarget = document.getElementById("site-header");
   if (!headerTarget) return;
-
   try {
-    const response = await fetch("header.html");
+    const depth = location.pathname.split('/').length - 2;
+    const prefix = depth > 1 ? '../'.repeat(depth - 1) : '';
+    const response = await fetch(prefix + 'header.html');
     const html = await response.text();
     headerTarget.innerHTML = html;
   } catch (error) {
